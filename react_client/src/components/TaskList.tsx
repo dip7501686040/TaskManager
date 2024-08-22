43c0c30f3d5cf8ab7f69eb6715044a29a3404399
dip7501686040 <dip7501686040@gmail.com>
2024-08-23 01:31:36 +0530

    Updated

--- react_client/src/components/TaskList.tsx
+++ react_client/src/components/TaskList.tsx
@@ -20,7 +20,7 @@ function TaskList() {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: "warning",
+      icon: "warning!",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
