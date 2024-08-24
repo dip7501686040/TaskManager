e8c2618d365c62de0e3a22a765a667851bc331f4
dip7501686040 <dip7501686040@gmail.com>
2024-08-24 09:41:23 +0530

    removed logo

--- react_client/src/App.tsx
+++ react_client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import TaskList from "./components/TaskList"
 import TaskForm from "./components/TaskForm"
